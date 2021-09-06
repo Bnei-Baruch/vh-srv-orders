cdd1b0c67006d2d84ec03f8e2a7f301ca71159df
Yasha <3-yasha@users.noreply.gitlab.bbdev.team>
2021-09-06 21:08:14 +0200

    special membership

--- status_handler.go
+++ status_handler.go
@@ -11,25 +11,33 @@ func Status(c *gin.Context) {
 	filter := string(c.Params.ByName("email"))
 	paidmb := hasPaidMembership(filter)
 	ticket := hasTicket(filter)
+	specialmb := hasSpecialMembership(filter)
 
-	mb := paidmb
+	var mb bool
 
 	mbLabel := ""
 	mbColor := ""
 
-	if mb {
+	if paidmb {
+		mb = true
 		mbLabel = "active"
 		mbColor = "34A853"
-	} else {
-		mbLabel = "inactive"
-		mbColor = "5F6368"
+		c.JSON(http.StatusOK, gin.H{"membership": mb, "ticket": ticket, "status_name": mbLabel, "status_color": mbColor})
+		return
 	}
-
+	if specialmb {
+		mb = true
+		mbLabel = "active"
+		mbColor = "2980b9"
+		c.JSON(http.StatusOK, gin.H{"membership": mb, "ticket": ticket, "status_name": mbLabel, "status_color": mbColor})
+		return
+	}
+	mb = false
+	mbLabel = "inactive"
+	mbColor = "5F6368"
 	c.JSON(http.StatusOK, gin.H{"membership": mb, "ticket": ticket, "status_name": mbLabel, "status_color": mbColor})
-}
+	return
 
-func hasSpecialMembership(email string) bool {
-	return false
 }
 
 func hasPaidMembership(email string) bool {
@@ -60,6 +68,23 @@ where a."Email" = ?
 and o."AccountID" = a.id
 and o."ProductType" = 'sept2021ticket'
 and (o."Status" = 'paid' or o."Status" = 'success')
+`
+	type Results struct {
+		Total int
+	}
+	var r Results
+	//var count map[string]interface{}
+	//DB.Raw(query, email).Scan(&r)
+	DB.Raw(query, email).Scan(&r)
+
+	return r.Total > 0
+}
+
+func hasSpecialMembership(email string) bool {
+	query := `
+select count(s.*) as total
+from specials as s
+where s."email" = ?
 `
 	type Results struct {
 		Total int

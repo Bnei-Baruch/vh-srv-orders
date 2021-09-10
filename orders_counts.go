57bf1bbbd5723a811827f5bc964809ae235c981d
Yasha <3-yasha@users.noreply.gitlab.bbdev.team>
2021-09-10 14:35:12 +0200

    convention count

--- orders_counts.go
+++ orders_counts.go
@@ -24,6 +24,8 @@ select count(o.*) as total
 from orders as o
 where o."ProductType" = 'globalmembership'
 and (o."Status" = 'paid' or o."Status" = 'success')
+and o.created_at > '2021-09-03'
+and (select count(q.id) from orders as q where q."AccountID" = o."AccountID" and o."Status" = 'paid') < 2
 `
 	type Results struct {
 		Total int64

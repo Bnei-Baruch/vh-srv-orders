810b4dfbcd3d97dfce23f5a0f063969a96e42043
Abhinav <abhinavshm95@gmail.com>
2022-04-29 01:06:31 +0530

    feat: migration pelecard_payment.sql

---
+++ db/m2_pelecard_payment.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS payments_offline (
+    id                        SERIAL PRIMARY KEY,
+    payment_method            TEXT NOT NULL,
+    receipt                   TEXT,
+    extra_info                TEXT,
+    status                    TEXT,
+    payment_id                INT NOT NULL,
+    created_at                TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    updated_at                TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    deleted_at                TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    CONSTRAINT fk_payment_id  FOREIGN KEY(payment_id) REFERENCES payments(id)
+);
+
+COMMIT;

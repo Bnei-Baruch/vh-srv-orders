c47ed7a528981d47275f6d064ef16839435761a5
Abhinav <abhinavshm95@gmail.com>
2022-09-07 21:20:17 +0530

    feat: update sql migration with unique constraint

--- db/migrations/06_user_payment_details_table.up.sql
+++ db/migrations/06_user_payment_details_table.up.sql
@@ -2,15 +2,16 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS payment_details (
     id                          SERIAL PRIMARY KEY,
-    cc_number                   TEXT NOT NULL UNIQUE,
-    cc_expdate                  TEXT NOT NULL UNIQUE,
-    account_id                  INT NOT NULL UNIQUE,
-    gateway_provider            TEXT NOT NULL UNIQUE,
-    active                      BOOLEAN NOT NULL UNIQUE,
+    cc_number                   TEXT NOT NULL,
+    cc_expdate                  TEXT NOT NULL,
+    account_id                  INT NOT NULL,
+    gateway_provider            TEXT NOT NULL,
+    active                      BOOLEAN NOT NULL,
     created_at                  TIMESTAMP WITH TIME ZONE DEFAULT now(),
     updated_at                  TIMESTAMP WITH TIME ZONE DEFAULT now(),
     deleted_at                  TIMESTAMP WITH TIME ZONE DEFAULT null,
-    CONSTRAINT fk_account_id    FOREIGN KEY(account_id) REFERENCES accounts(id)
+    CONSTRAINT fk_account_id    FOREIGN KEY(account_id) REFERENCES accounts(id),
+    CONSTRAINT uq_cc_number_account_id_key UNIQUE (cc_number, account_id)
 );
 
 COMMIT;

e9f025b159e5f3ac84eb3b6c8b684707034f4d74
Abhinav <abhinavshm95@gmail.com>
2022-09-20 16:50:15 +0530

    feat: initial struct down

---
+++ db/migrations/00_initial_struct.down.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+DROP TABLE IF EXISTS specials_sep2021;
+
+DROP TABLE IF EXISTS specials;
+
+DROP TABLE IF EXISTS payments_pelecard;
+
+DROP TABLE IF EXISTS payments_offline;
+
+DROP TABLE IF EXISTS payments_helphaver;
+
+DROP TABLE IF EXISTS payments;
+
+DROP TABLE IF EXISTS orders;
+
+DROP TABLE IF EXISTS invoices;
+
+DROP TABLE IF EXISTS accounts;
+
+COMMIT;
